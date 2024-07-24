d57b6839d8fc9cbcf76dddb5b256cf730e8c61be
BillGatesReckoning <rusinkost@gmail.com>
2024-07-24 16:16:12 +0200

    Add operate function and number and operator variables

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let numberA
+let numberB
+let operator
+
+function operate(numberA, numberB, operator) {
+    switch(operator) {
+        case "+":
+            return add(numberA, numberB)
+        case "-":
+            return subtract(numberA, numberB)
+        case "*":
+            return multiply(numberA, numberB)
+        case "/":
+            return divide(numberA, numberB)
+    }
+}
+
 function add(numberA, numberB) {
     return numberA + numberB
 }
