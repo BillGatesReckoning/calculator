c5802d071a7595a551dccb50add706d7817d0438
BillGatesReckoning <rusinkost@gmail.com>
2024-07-24 18:57:27 +0200

    Add all the calculator logic

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-let numberA
-let numberB
-let operator
+let numberA = NaN
+let numberB = NaN
+let operator = ""
+let operatorWorks = false
 
 let displayValue = ""
 
 const numberButtons = document.querySelectorAll(".numberButton")
+const operatorButtons = document.querySelectorAll(".operatorButton")
+const evaluationButton = document.querySelector(".evaluationButton")
+const clearButton = document.querySelector(".clearButton")
 const display = document.querySelector(".display")
 
 numberButtons.forEach(button => 
     button.addEventListener("click", (e) => {
         displayValue += `${e.currentTarget.textContent}`
         display.textContent = displayValue
+        if (isNaN(numberA)) operatorWorks = true
     }))
 
+operatorButtons.forEach(button => 
+    button.addEventListener("click", (e) => {
+        if (operatorWorks === false) return
+        numberA = displayValue
+        operator = e.currentTarget.textContent
+        displayValue = ""
+        display.textContent = `${operator}` + " "
+        operatorWorks = false
+    })
+)
 
+evaluationButton.addEventListener("click", () => {
+    if (numberA === NaN || numberB === NaN || operator === "") return
+    numberB = +displayValue
+    if (numberB === 0 && operator === "/") {
+        display.textContent = "You can't divide with zero!"
+    }
+    else {
+        display.textContent = operate(+numberA, +numberB, operator)
+    }
+    numberA = NaN
+    numberB = NaN
+    operator = ""
+    displayValue = +display.textContent
+    operatorWorks = true
+})
 
-
+clearButton.addEventListener("click", () => {
+    numberA = NaN
+    numberB = NaN
+    operator = ""
+    displayValue = ""
+    display.textContent = ""
+    operatorWorks = false
+})
 
 function operate(numberA, numberB, operator) {
     switch(operator) {
